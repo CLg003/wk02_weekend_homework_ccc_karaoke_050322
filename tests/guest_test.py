52a19b167692b2007d695fd18a3daedb84ab3888
CLg003 <clairemjlaing@gmail.com>
2022-03-05 12:41:53 +0000

    added test_guest_has_name and test_can_update_guest_name

--- tests/guest_test.py
+++ tests/guest_test.py
@@ -3,7 +3,11 @@ from classes.guest import Guest
 
 class TestGuest(unittest.TestCase):
     def setUp(self):
-        pass
+        self.guest_1 = Guest("Sandy")
 
     def test_guest_has_name(self):
-        pass
+        self.assertEqual("Sandy", self.guest_1.name)
+
+    def test_can_update_guest_name(self):
+        self.guest_1.name = "Sandra"
+        self.assertEqual("Sandra", self.guest_1.name)
