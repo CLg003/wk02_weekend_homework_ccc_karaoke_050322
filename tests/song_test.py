a737a870e344c4c7b16532b34dcdc1b77941629f
CLg003 <clairemjlaing@gmail.com>
2022-03-05 12:50:32 +0000

    added test_song_has_artist and test_can_update_song_artist

--- tests/song_test.py
+++ tests/song_test.py
@@ -11,3 +11,11 @@ class TestSong(unittest.TestCase):
     def test_can_update_song_title(self):
         self.song_1.title = "Night Fever"
         self.assertEqual("Night Fever", self.song_1.title)
+
+    def test_song_has_artist(self):
+        self.assertEqual("Bee Gees", self.song_1.artist)
+
+    # @unittest.skip("delete this line to run the test")
+    def test_can_update_song_artist(self):
+        self.song_1.artist = "John Travolta"
+        self.assertEqual("John Travolta", self.song_1.artist)
