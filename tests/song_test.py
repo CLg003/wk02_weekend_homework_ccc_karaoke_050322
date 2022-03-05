d4a2ad49d6d3c02af07b2a512a2fcbb44af66cd4
CLg003 <clairemjlaing@gmail.com>
2022-03-05 12:46:56 +0000

    added test_song_has_title and test_can_update_song_title

--- tests/song_test.py
+++ tests/song_test.py
@@ -3,7 +3,11 @@ from classes.song import Song
 
 class TestSong(unittest.TestCase):
     def setUp(self):
-        pass
+        self.song_1 = Song("Stayin' Alive", "Bee Gees")
 
     def test_song_has_title(self):
-        pass
+        self.assertEqual("Stayin' Alive", self.song_1.title)
+
+    def test_can_update_song_title(self):
+        self.song_1.title = "Night Fever"
+        self.assertEqual("Night Fever", self.song_1.title)
